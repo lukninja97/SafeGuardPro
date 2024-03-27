273549b409a3977780c086e0d3a302ef1248d057
lukninja97 <lucassilvasouto@gmail.com>
2024-03-26 21:46:46 -0300

    Criar a fragment de Entrega.

---
+++ app/src/main/java/com/lukninja/safeguardpro/view/EntregaFragment.kt
@@ -0,0 +1,28 @@
+package com.lukninja.safeguardpro.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.lukninja.safeguardpro.R
+import com.lukninja.safeguardpro.databinding.FragmentEntregaBinding
+import com.lukninja.safeguardpro.databinding.FragmentFuncionariosBinding
+
+class EntregaFragment : Fragment() {
+
+    private var _binding: FragmentEntregaBinding? = null
+    private val binding: FragmentEntregaBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentEntregaBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
