465820f7eb07421d314f4a0cc43fd9999fba98a5
lukninja97 <lucassilvasouto@gmail.com>
2024-05-31 20:00:41 -0300

    Criar a fragment da tela inicial do supervisor.

---
+++ app/src/main/java/com/lukninja/safeguardpro/view/HomeSupervisorFragment.kt
@@ -0,0 +1,29 @@
+package com.lukninja.safeguardpro.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.lukninja.safeguardpro.R
+import com.lukninja.safeguardpro.databinding.FragmentHomeBinding
+import com.lukninja.safeguardpro.databinding.FragmentHomeSupervisorBinding
+
+
+class HomeSupervisorFragment : Fragment() {
+    private var _binding: FragmentHomeSupervisorBinding? = null
+    private val binding: FragmentHomeSupervisorBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeSupervisorBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
