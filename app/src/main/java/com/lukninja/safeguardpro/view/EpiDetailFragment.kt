d27c4d0da6edc0721bcd59d23547039c33ae0ae1
lukninja97 <lucassilvasouto@gmail.com>
2024-05-31 19:58:06 -0300

    Criar a fragment de detalhes de epi.

---
+++ app/src/main/java/com/lukninja/safeguardpro/view/EpiDetailFragment.kt
@@ -0,0 +1,27 @@
+package com.lukninja.safeguardpro.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.lukninja.safeguardpro.databinding.FragmentEpiDetailBinding
+
+class EpiDetailFragment : Fragment() {
+
+    private var _binding: FragmentEpiDetailBinding? = null
+    private val binding: FragmentEpiDetailBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentEpiDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
