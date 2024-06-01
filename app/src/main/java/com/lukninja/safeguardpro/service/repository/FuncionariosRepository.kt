12f9fec8050869717245f106472ff350ca954186
lukninja97 <lucassilvasouto@gmail.com>
2024-05-31 21:30:36 -0300

    configurar a parte do funcionario para receber a api.

---
+++ app/src/main/java/com/lukninja/safeguardpro/service/repository/FuncionariosRepository.kt
@@ -0,0 +1,7 @@
+package com.lukninja.safeguardpro.service.repository
+
+import android.content.Context
+
+class FuncionariosRepository(context: Context) {
+
+}
--- app/src/main/java/com/lukninja/safeguardpro/view/FuncionariosFragment.kt
+++ app/src/main/java/com/lukninja/safeguardpro/view/FuncionariosFragment.kt
@@ -5,12 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.lukninja.safeguardpro.R
 import com.lukninja.safeguardpro.databinding.FragmentFuncionariosBinding
+import com.lukninja.safeguardpro.view.adapter.FuncionarioAdapter
+import com.lukninja.safeguardpro.viewmodel.FuncionariosViewModel
 
 class FuncionariosFragment : Fragment() {
 
     private var _binding: FragmentFuncionariosBinding? = null
     private val binding: FragmentFuncionariosBinding get() = _binding!!
+
+    private val viewModel: FuncionariosViewModel by viewModels()
+    private lateinit var adapter: FuncionarioAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,5 +31,29 @@ class FuncionariosFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        //Quando clicar em algum item da lista
+        adapter = FuncionarioAdapter(viewModel.funcionarioList.value) { funcionario ->
+            val pessoaBundle = Bundle()
+            pessoaBundle.putInt("pessoaId", funcionario.id)
+            arguments = pessoaBundle
+            findNavController().navigate(R.id.funcionarioDetailFragment, arguments)
+        }
+
+        // Configura a recycler
+        val recycler = binding.rvFuncionarios
+        recycler.layoutManager = LinearLayoutManager(requireContext())
+        recycler.adapter = adapter
+
+        // Observa para adicionar um item na lista quando for adicionado
+        viewModel.funcionarioList.observe(viewLifecycleOwner) {
+            adapter.updateFuncionarios(it)
+        }
+
+        binding.btnAdd.setOnClickListener {
+            findNavController().navigate(R.id.funcionarioFragment)
+        }
+
+        //Carrega os funcionarios e popula a lista
+        viewModel.load()
     }
 }
